37840d25ceaeb5d573d15e9ad5a6c867c4bf721a
Arnaud BEUX <34894524+Nytuo@users.noreply.github.com>
2022-01-06 10:13:17 +0100

    1

--- version2.c
+++ version2.c
@@ -2,51 +2,55 @@
 // Created by ba101397 on 1/6/2022.
 //
 #include "entetesVersion2.h"
+
 void version2(int grille[10][10], int largeur, int cible, solution *tabSolutions, int *taille) {
 //boucle pour parcourir la liste
-cheking(grille,3,3, largeur, cible, tabSolutions, taille);
+    cheking(grille, 3, 3, largeur, cible, tabSolutions, taille,59);
 
 }
 
-void cheking(int grille[10][10],int y,int x, int position, int largeur, solution *tabSolutions, int *taille, int cible) {
+void
+cheking(int grille[10][10], int y, int x, int position, int largeur, solution *tabSolutions, int *taille, int cible) {
 
 
 
     //on va droite
-    if(x+2>largeur){
-        if (grille[y][x]*grille[y][x+1]+grille[y][x+2]== cible) {
+    if (x + 2 > largeur) {
+        if (grille[y][x] * grille[y][x + 1] + grille[y][x + 2] == cible) {
             tabSolutions[*taille].a = grille[y][x];
-            tabSolutions[*taille].b = grille[y][x+1];
-            tabSolutions[*taille].c = grille[y][x+2];
+            tabSolutions[*taille].b = grille[y][x + 1];
+            tabSolutions[*taille].c = grille[y][x + 2];
             tabSolutions[*taille].cible = cible;
         }
 
     }
     //on va gauche
-    if (grille[y][x]*grille[y][x-1]+grille[y][x-2]== cible) {
-        tabSolutions[*taille].a = grille[y][x];
-        tabSolutions[*taille].b = grille[y][x-1];
-        tabSolutions[*taille].c = grille[y][x-2];
-        tabSolutions[*taille].cible = cible;
+    if (x + 2 < 0) {
+        if (grille[y][x] * grille[y][x - 1] + grille[y][x - 2] == cible) {
+            tabSolutions[*taille].a = grille[y][x];
+            tabSolutions[*taille].b = grille[y][x - 1];
+            tabSolutions[*taille].c = grille[y][x - 2];
+            tabSolutions[*taille].cible = cible;
+        }
     }
     //on va bas
-    if (grille[y][x]*grille[y+1][x]+grille[y+2][x]== cible) {
-        tabSolutions[*taille].a = grille[y][x];
-        tabSolutions[*taille].b = grille[y+1][x];
-        tabSolutions[*taille].c = grille[y+2][x];
-        tabSolutions[*taille].cible = cible;
+    if (y + 2 > largeur) {
+        if (grille[y][x] * grille[y + 1][x] + grille[y + 2][x] == cible) {
+            tabSolutions[*taille].a = grille[y][x];
+            tabSolutions[*taille].b = grille[y + 1][x];
+            tabSolutions[*taille].c = grille[y + 2][x];
+            tabSolutions[*taille].cible = cible;
+        }
     }
     //on va haut
-    if (grille[y][x]*grille[y-1][x]+grille[y-2][x]== cible) {
-        tabSolutions[*taille].a = grille[y][x];
-        tabSolutions[*taille].b = grille[y-1][x];
-        tabSolutions[*taille].c = grille[y-2][x];
-        tabSolutions[*taille].cible = cible;
+    if (y + 2 < 0) {
+        if (grille[y][x] * grille[y - 1][x] + grille[y - 2][x] == cible) {
+            tabSolutions[*taille].a = grille[y][x];
+            tabSolutions[*taille].b = grille[y - 1][x];
+            tabSolutions[*taille].c = grille[y - 2][x];
+            tabSolutions[*taille].cible = cible;
+        }
     }
-
-
-
-
 
 
 }
